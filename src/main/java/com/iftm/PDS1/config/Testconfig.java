c6b1ace68b762e6a62a483f6f03b9b8d32416a4c
Lorena <lorena.martins.1600@gmail.com>
2021-01-18 23:06:17 -0300

    OrderStatus enum

--- src/main/java/com/iftm/PDS1/config/Testconfig.java
+++ src/main/java/com/iftm/PDS1/config/Testconfig.java
@@ -10,6 +10,7 @@ import com.iftm.PDS1.entities.User;
 import com.iftm.PDS1.entities.Order;
 import com.iftm.PDS1.repositories.UserRepository;
 import com.iftm.PDS1.repositories.OrderRepository;
+import com.iftm.PDS1.entities.enums.OrderStatus;
 
 @Configuration
 @Profile("test")
@@ -28,11 +29,11 @@ public class Testconfig implements CommandLineRunner {
 
 		User u2 = new User(null, "Mariano Green", "mariano@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1, OrderStatus.PAID);
 		
-		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2, OrderStatus.WAITING_PAYMENT);
 		
-		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1, OrderStatus.WAITING_PAYMENT);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
--- src/main/java/com/iftm/PDS1/entities/Order.java
+++ src/main/java/com/iftm/PDS1/entities/Order.java
@@ -11,6 +11,8 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import com.iftm.PDS1.entities.enums.OrderStatus;
+
 @Entity
 @Table(name = "tb_order")
 public class Order implements Serializable {
@@ -25,15 +27,17 @@ public class Order implements Serializable {
 
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -59,6 +63,16 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
