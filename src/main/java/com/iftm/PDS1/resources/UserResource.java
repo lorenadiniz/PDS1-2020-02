9c16234012fe33be65d80bbcc6ce5c505451b5a1
Lorena <lorena.martins.1600@gmail.com>
2021-01-19 22:44:28 -0300

    User insert

--- src/main/java/com/iftm/PDS1/resources/UserResource.java
+++ src/main/java/com/iftm/PDS1/resources/UserResource.java
@@ -5,11 +5,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.iftm.PDS1.entities.User;
 import com.iftm.PDS1.services.UserService;
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -31,4 +35,15 @@ public class UserResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
+	
+	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+
+
 }
