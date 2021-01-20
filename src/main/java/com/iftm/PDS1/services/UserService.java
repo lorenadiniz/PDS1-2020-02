18bb4247207579020e0b1a272f1e3362c48c5bf4
Lorena <lorena.martins.1600@gmail.com>
2021-01-20 19:51:26 -0300

    Exception handling - update

--- src/main/java/com/iftm/PDS1/services/UserService.java
+++ src/main/java/com/iftm/PDS1/services/UserService.java
@@ -2,6 +2,9 @@ package com.iftm.PDS1.services;
 
 import java.util.List;
 import java.util.Optional;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -31,13 +34,13 @@ public class UserService {
 	}
 
 	public void delete(Long id) {
-		
+
 		try {
 			repository.deleteById(id);
-		} catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		} catch(DataIntegrityViolationException e) {
-			throw new DatabaseException(e.getMessage()); 
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
 		}
 
 	}
@@ -52,9 +55,13 @@ public class UserService {
 
 	public User update(Long id, User obj) {
 
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getOne(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 
 	}
 
