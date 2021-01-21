0ea1f23944554180460eccdca0f3a51d14f4e007
Lorena <lorena.martins.1600@gmail.com>
2021-01-20 23:54:31 -0300

    Product insert

--- src/main/java/com/iftm/PDS1/resources/ProductResource.java
+++ src/main/java/com/iftm/PDS1/resources/ProductResource.java
@@ -1,7 +1,9 @@
 package com.iftm.PDS1.resources;
 
+import java.net.URI;
 import java.util.List;
 
+import com.iftm.PDS1.dto.ProductCategoriesDTO;
 import com.iftm.PDS1.dto.ProductDTO;
 import com.iftm.PDS1.services.ProductService;
 
@@ -9,8 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
 @RestController
@@ -34,5 +39,12 @@ public class ProductResource {
 	
 	}
 
+	@PostMapping
+	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
+		ProductDTO newDto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+					.buildAndExpand(newDto.getId()).toUri();
+		return ResponseEntity.created(uri).body(newDto);
+	}
 	
 }
--- src/main/java/com/iftm/PDS1/services/ProductService.java
+++ src/main/java/com/iftm/PDS1/services/ProductService.java
@@ -1,14 +1,21 @@
 package com.iftm.PDS1.services;
 
-import com.iftm.PDS1.dto.ProductDTO;
-import com.iftm.PDS1.entities.Product;
-import com.iftm.PDS1.repositories.ProductRepository;
-import com.iftm.PDS1.services.exceptions.ResourceNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.iftm.PDS1.dto.CategoryDTO;
+import com.iftm.PDS1.dto.ProductCategoriesDTO;
+import com.iftm.PDS1.dto.ProductDTO;
+import com.iftm.PDS1.entities.Category;
+import com.iftm.PDS1.entities.Product;
+import com.iftm.PDS1.repositories.CategoryRepository;
+import com.iftm.PDS1.repositories.ProductRepository;
+import com.iftm.PDS1.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class ProductService {
@@ -16,9 +23,11 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 
-	
+	@Autowired
+	private CategoryRepository categoryRepository;
+
 	public List<ProductDTO> findAll() {
-		List<Product> list =  repository.findAll();
+		List<Product> list = repository.findAll();
 		return list.stream().map(e -> new ProductDTO(e)).collect(Collectors.toList());
 
 	}
@@ -29,5 +38,19 @@ public class ProductService {
 		return new ProductDTO(entity);
 	}
 
-	
+	@Transactional
+	public ProductDTO insert(ProductCategoriesDTO dto) {
+		Product entity = dto.toEntity();
+		setProductCategories(entity, dto.getCategories());
+		entity = repository.save(entity);
+		return new ProductDTO(entity);
+	}
+
+	private void setProductCategories(Product entity, List<CategoryDTO> categories) {
+		entity.getCategories().clear();
+		for (CategoryDTO dto : categories) {
+			Category category = categoryRepository.getOne(dto.getId());
+			entity.getCategories().add(category);
+		}
+	}
 }
