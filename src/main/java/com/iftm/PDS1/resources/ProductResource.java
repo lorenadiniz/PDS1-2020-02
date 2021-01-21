76d102924119d6fdd0a8a92bcc5db3cfd955a758
Lorena <lorena.martins.1600@gmail.com>
2021-01-21 00:28:30 -0300

    Product update

--- src/main/java/com/iftm/PDS1/resources/ProductResource.java
+++ src/main/java/com/iftm/PDS1/resources/ProductResource.java
@@ -12,11 +12,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
 
 @RestController
 @RequestMapping(value = "/products")
@@ -26,9 +26,9 @@ public class ProductResource {
 	private ProductService service;
 
 	@GetMapping
-	public ResponseEntity<List<ProductDTO>> findAll() {	
+	public ResponseEntity<List<ProductDTO>> findAll() {
 		List<ProductDTO> list = service.findAll();
-		
+
 		return ResponseEntity.ok().body(list);
 	}
 
@@ -36,15 +36,22 @@ public class ProductResource {
 	public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
 		ProductDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
-	
+
 	}
 
 	@PostMapping
 	public ResponseEntity<ProductDTO> insert(@RequestBody ProductCategoriesDTO dto) {
 		ProductDTO newDto = service.insert(dto);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-					.buildAndExpand(newDto.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
 		return ResponseEntity.created(uri).body(newDto);
 	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductCategoriesDTO dto) {
+		
+		ProductDTO newDto = service.update(id, dto);
+		return ResponseEntity.ok().body(newDto);
 	
+	}
+
 }
